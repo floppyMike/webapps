ac7ee3a0d553c3f97939140748b631f99a60eacb
Michaellu <mchblch@protonmail.com>
2024-06-29 22:22:53 +0200

    init

---
+++ string-matching.js
@@ -0,0 +1,61 @@
+const inputForm = document.getElementById("matchingForm");
+const parseTable = document.getElementById('parseTable');
+const needleInput = document.getElementById('needleInput');
+const haystackInput = document.getElementById('haystackInput');
+const stringMatchTable = document.getElementById('stringMatchTable');
+const seperator = document.getElementById('seperator');
+
+function tcreateRow(headStr, rowStr) {
+	const tr = document.createElement('tr');
+
+	const th = document.createElement('th');
+	th.scope = 'col';
+	th.textContent = headStr;
+	tr.appendChild(th);
+
+	for (let pair of rowStr) {
+		const td = document.createElement('td');
+		td.scope = 'col';
+
+		if (pair !== undefined) {
+			td.textContent = pair[0];
+			td.className = pair[1];
+		}
+
+		tr.appendChild(td);
+	}
+
+	return tr;
+}
+
+function tappendRow(parent, headStr, rowStr) {
+	const row = tcreateRow(headStr, rowStr);
+	parent.appendChild(row);
+}
+
+function genMatchTable(matchArray) {
+	const haystack = haystackInput.value;
+
+	stringMatchTable.innerHTML = '';
+
+	const table = document.createElement('table');
+	table.className = 'table table-bordered';
+
+	// Generate head row
+
+	const thead = document.createElement('thead');
+	tappendRow(thead, "#", haystack.split('').map(c => [c, "text-normal-emphasis"]));
+	table.appendChild(thead);
+
+	// Generate body
+
+	const tbody = document.createElement('tbody');
+	tbody.className = "table-group-divider";
+
+	for (let i = 0; i < matchArray.length; ++i) {
+		tappendRow(tbody, i, matchArray[i]);
+	}
+
+	table.appendChild(tbody);
+	stringMatchTable.appendChild(table);
+}
